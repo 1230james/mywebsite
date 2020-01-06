e0859c501a30b605c7b4da882a877c8a872eba6f
James Hyun <jameshyun1230@gmail.com>
2020-01-06 01:35:55 -0500

    Removed "index.html" from Home link in navbar (it's unnecessary)

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 $("#navbar").html(`
-    <a href="/index.html">Home</a>
+    <a href="">Home</a>
     <a href="">About Me</a>
     <a href="">Half Past Noon</a>
     <a href="">Programming Projects</a>
