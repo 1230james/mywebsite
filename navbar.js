778883e870edd4597145044c3f3463334f6bec49
James Hyun <jameshyun1230@gmail.com>
2020-01-06 01:34:16 -0500

    Added [dud] links to planned pages

--- navbar.js
+++ navbar.js
@@ -1,3 +1,8 @@
-document.getElementById("navbar").innerHTML = `
+$("#navbar").html(`
     <a href="/index.html">Home</a>
-`
+    <a href="">About Me</a>
+    <a href="">Half Past Noon</a>
+    <a href="">Programming Projects</a>
+    <a href="">Sheet Music</a>
+    <a href="">Contact</a>
+`);
