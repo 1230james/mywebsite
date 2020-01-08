cd0bcfca36793d702fc5396b8a6d768498a11165
James Hyun <jameshyun1230@gmail.com>
2020-01-08 01:34:41 -0500

    Finished seamless page changes

--- navigation.js
+++ navigation.js
@@ -1,20 +1,27 @@
 // Navigation Bar & Seamless page loading
 "use strict";
 
+// Configureables
+const animationTime = 750;
+const timeBetweenAnim = animationTime + 250; // only change the number
+
+// Variables
 const sep = "<b><b>|</b></b>";
 const navlinks = {
-    "Home": "file:///C:/Users/james/Desktop/ProgrammingProjects/1230james.github.io/index.html",
-    "About Me": "file:///C:/Users/james/Desktop/ProgrammingProjects/1230james.github.io/about/index.html",
-    "Half Past Noon": "/",
-    "Programming Projects": "/",
-    "Sheet Music": "/",
-    "Contact": "/",
+    "Home": "/",
+    "About Me": "/about",
+    "Half Past Noon": "/hpn",
+    "Programming Projects": "/programming",
+    "Sheet Music": "/music",
+    "Contact": "/contact",
 }
+var debounce = false
+var currentPath = location.pathname;
 
 // Run on ready
 $(document).ready(function() {
     // Create nav links
-    let str = "";
+    let str = `${sep} `;
     for (let text in navlinks) {
         str += `<a href="${navlinks[text]}">${text}</a> ${sep} `;
     }
@@ -24,30 +31,41 @@ $(document).ready(function() {
     $("#navbar").children().each(function(i,e) {
         if (navlinks[e.innerText] != null) {
             e.onclick = function() {
-                console.log("Clicked " + e.innerText);
-                console.log("Path to new page: " + navlinks[e.innerText]);
-                history.pushState({ path: this.path }, '', navlinks[e.innerText]);
-                loadPage(navlinks[e.innerText]);
+                if (debounce) return false;
+                history.pushState({}, '', navlinks[e.innerText]);
+                loadPage();
                 return false;
             };
         }
     });
     
     $(window).bind('popstate', function() {
-        history.go(-1);
+        loadPage(location.pathname);
     });
 });
 
 // Load page (incl. animations)
-function loadPage(url) {
-    console.log("Loading page!");
+function loadPage() {
+    
+    // Debounce + Same page check (return if the page is the same)
+    if (debounce || location.pathname == currentPath) return;
+    else debounce = true; 
+    
+    let content = $("#content");
+    
     // "Unload" animation
-        // TODO
+    content.animate({paddingTop: "+=300px", opacity: "0"}, animationTime);
     
-    // Update page
-    $("#content").load(url + " #content");
+    // Wait half a second
+    setTimeout(function() {
+        // Update page
+        currentPath = location.pathname;
+        content.load(currentPath + " #content");
+        // Load animation
+        content.animate({paddingTop: "-=300px", opacity: "1"}, animationTime);
+        // Debounce
+        setTimeout(function() { debounce = false; }, animationTime);
+    }, timeBetweenAnim);
     
-    // Load animation
-        // TODO
 }
 
