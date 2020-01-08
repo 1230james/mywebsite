639d30da1977da2c354c12e3ce9624d573679273
James Hyun <jameshyun1230@gmail.com>
2020-01-07 23:45:51 -0500

    Functional seamless loading (not ready for master)

--- navigation.js
+++ navigation.js
@@ -3,8 +3,8 @@
 
 const sep = "<b><b>|</b></b>";
 const navlinks = {
-    "Home": "/",
-    "About Me": "/",
+    "Home": "file:///C:/Users/james/Desktop/ProgrammingProjects/1230james.github.io/index.html",
+    "About Me": "file:///C:/Users/james/Desktop/ProgrammingProjects/1230james.github.io/about/index.html",
     "Half Past Noon": "/",
     "Programming Projects": "/",
     "Sheet Music": "/",
@@ -24,42 +24,30 @@ $(document).ready(function() {
     $("#navbar").children().each(function(i,e) {
         if (navlinks[e.innerText] != null) {
             e.onclick = function() {
-                history.pushState({ path: this.path }, '', this.href);
-                $.get(this.href, function(data) {
-                    console.log(data);
-                });
+                console.log("Clicked " + e.innerText);
+                console.log("Path to new page: " + navlinks[e.innerText]);
+                history.pushState({ path: this.path }, '', navlinks[e.innerText]);
+                loadPage(navlinks[e.innerText]);
                 return false;
             };
         }
     });
+    
+    $(window).bind('popstate', function() {
+        history.go(-1);
+    });
 });
 
-/*
-
-// Smooth loading functions
-
-// Page load
+// Load page (incl. animations)
 function loadPage(url) {
+    console.log("Loading page!");
+    // "Unload" animation
+        // TODO
     
+    // Update page
+    $("#content").load(url + " #content");
+    
+    // Load animation
+        // TODO
 }
 
-// On click
-function handleClicked(element) {
-    if (navlinks[element.innerText]) {
-        loadPage(navlinks[element.innerText]);
-        return false; // stop default behavior
-    }
-    return true;
-}
-
-// On click event
-$(document).onclick = function(e) {
-    // https://stackoverflow.com/questions/1760096/override-default-behaviour-for-link-a-objects-in-javascript
-    e = e || window.event;
-    let element = e.target || e.srcElement;
-    if (element.tagName == "A") {
-        return handleClicked(element);
-    }
-}
-
-*/
