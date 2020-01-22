64cde8acdde2a98c730b27c8a4d21f0e079aaf75
James Hyun <jameshyun1230@gmail.com>
2020-01-22 05:12:47 -0500

    (Hopefully) finished local navigation

--- navigation.js
+++ navigation.js
@@ -4,6 +4,7 @@
 // Configureables
 const animationTime = 750;
 const timeBetweenAnim = 250; // only change the number
+const animationDistance = 300; // # of pixels to slide content by when changing page
 
 // Variables
 const sep = "<b><b>|</b></b>";
@@ -15,11 +16,19 @@ const navlinks = {
     "Sheet Music": "/music",
     "Contact": "/contact",
 }
-var debounce = false
+var debounce = false;
+var isUnloadAnimationPlaying = false;
 var currentPath;
+var contentPaddingTopHigh; // too bad they can't be consts qq
+var contentPaddingTopLow;
 
 // Run on ready
 $(document).ready(function() {
+    // Set contentPaddingTop values
+    contentPaddingTopHigh = $("#content").css("paddingTop");
+    contentPaddingTopLow = (parseInt(contentPaddingTopHigh.replace(/\D/g,''))
+        + animationDistance) + "px";
+    
     // Create nav links
     fixLinks();
     let str = `${sep} `;
@@ -33,20 +42,29 @@ $(document).ready(function() {
     $("a").each(function(i,e) {
         e.onclick = function() {
             if (isLocalAnchor(e)) {
-                if (debounce) return false; else debounce = true;
                 history.pushState({}, '', navlinks[e.innerText]);
-                loadPage();
+                if (!debounce) {
+                    debounce = true;
+                    loadPage();
+                } else {
+                    return false;
+                }
                 return false;
             } else return true;
         };
     });
     
     $(window).bind('popstate', function() {
-        if (debounce) {
-            loadContent();
-        } else {
+        if (!isUnloadAnimationPlaying) {
             debounce = true;
             loadPage();
+        } else {
+            setInterval(function() { // Wait until unload anim is done
+                if (!isUnloadAnimationPlaying) {
+                    clearInterval();
+                    loadContent();
+                }
+            }, 100);
         }
     });
 });
@@ -57,11 +75,13 @@ function loadPage() {
     if (location.pathname == currentPath) return;
     
     // "Unload" animation
-    $("#content").animate({paddingTop: "+=300px", opacity: "0"}, animationTime, "swing", function() {
+    isUnloadAnimationPlaying = true
+    $("#content").animate({paddingTop: contentPaddingTopLow, opacity: "0"}, animationTime, "swing", function() {
         setTimeout(function() {
             loadContent();
+            isUnloadAnimationPlaying = false;
             // Load animation
-            $("#content").animate({paddingTop: "-=300px", opacity: "1"}, animationTime, "swing", function() {
+            $("#content").animate({paddingTop: contentPaddingTopHigh, opacity: "1"}, animationTime, "swing", function() {
                 debounce = false;
             });
         }, timeBetweenAnim);
